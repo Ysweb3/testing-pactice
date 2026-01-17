40fbf3ae573ba357cbe68a07e9dac76232cef35b
Ysweb3 <yssh88972@gmail.com>
2026-01-18 01:13:11 +0530

    fix non-alphabetic characters to remain unchanged

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,13 +1,15 @@
-console.log(String.fromCharCode(65+1));
+console.log(String.fromCharCode(65+1));//B
 function caesarChipher(str, shift){
     let arr = str.split('');
     for(let i = 0; i < str.length; i++){
-      
-        let charCode = arr[i].charCodeAt();
-        charCode += shift;
-        arr[i] = String.fromCharCode(charCode);
+        if(arr[i].charCodeAt() >=65 && arr[i].charCodeAt() <= 90|| arr[i].charCodeAt() >=97 && arr[i].charCodeAt() <= 122){//A-Z,a-z
+            console.log(arr[i].charCodeAt());   
+            let charCode = arr[i].charCodeAt();
+            charCode += shift;
+            arr[i] = String.fromCharCode(charCode);
+        }
         
     }
     return arr.join('');
 }
-console.log(caesarChipher("Hello", 3)); 
+console.log(caesarChipher("Hello, World!", 3)); 
