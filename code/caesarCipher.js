67473f2dad29c04cd9f5c0aea0a8f087c066ec8e
Ysweb3 <yssh88972@gmail.com>
2026-01-18 21:07:15 +0530

    implemented caesarChipher with modulo for wrapping

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,15 +1,20 @@
-console.log(String.fromCharCode(65+1));//B
+console.log(((90-65+3+26)%26)+65);//B
 function caesarChipher(str, shift){
     let arr = str.split('');
     for(let i = 0; i < str.length; i++){
-        if(arr[i].charCodeAt() >=65 && arr[i].charCodeAt() <= 90|| arr[i].charCodeAt() >=97 && arr[i].charCodeAt() <= 122){//A-Z,a-z
-            console.log(arr[i].charCodeAt());   
-            let charCode = arr[i].charCodeAt();
-            charCode += shift;
+        shift = shift % 26;
+        if(arr[i].charCodeAt() >=65 && arr[i].charCodeAt() <= 90){//A-Z
+            let code = arr[i].charCodeAt();
+            let charCode = ((code-65+3+26)%26)+65;
+            arr[i] = String.fromCharCode(charCode);    
+        }
+        else if (arr[i].charCodeAt() >=97 && arr[i].charCodeAt() <= 122) {
+            let code = arr[i].charCodeAt();
+            let charCode = ((code-97+3+26)%26)+97;
             arr[i] = String.fromCharCode(charCode);
         }
-        
+       
     }
     return arr.join('');
 }
-console.log(caesarChipher("Hello, World!", 3)); 
+console.log(caesarChipher("xyz", 3)); 
